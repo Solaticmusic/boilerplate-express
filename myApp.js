6877372c09f336de3aaf123f7bd4e7c85a53faf4
Solaticmusic <Luca.Flechner@online.de>
2021-04-25 17:53:13 +0200

    Hello World

--- myApp.js
+++ myApp.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var app = express();
+// var express = require('express');
+// var app = express();
+
+console.log("Hello World");
 
 
 
@@ -34,6 +36,4 @@ var app = express();
 
 
 
-
-
- module.exports = app;
+//  module.exports = app;
